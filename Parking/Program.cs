d7a06f340c5dc6a9f84778229f9552ec658b615e
Lucas Ortis <lucasortis01@gmail.com>
2021-09-25 14:05:22 -0300

    Implementing Services class BrazilTaxService and RentalService

--- Parking/Program.cs
+++ Parking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using Parking.Entities;
 
 namespace Parking
 {
@@ -6,7 +8,22 @@ namespace Parking
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter rental data");
             
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            Console.WriteLine("Enter price per hour: ");
+            double priceHour = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter price per day: ");
+            double priceDay = double.Parse(Console.ReadLine());
+
         }
     }
 }
