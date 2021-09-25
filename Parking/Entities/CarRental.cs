eb1e79a852b98384c120052e4428b4782132f487
Lucas Ortis <lucasortis01@gmail.com>
2021-09-25 14:26:26 -0300

    Implementing ProcessInvoice method and finalizing the program

--- Parking/Entities/CarRental.cs
+++ Parking/Entities/CarRental.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Parking.Entities
 {
-    class Class1
+    class CarRental
     {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+        }
+
+
     }
 }
--- Parking/Program.cs
+++ Parking/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using Parking.Entities;
+using Parking.Services;
 
 namespace Parking
 {
@@ -13,16 +14,23 @@ namespace Parking
             Console.Write("Car model: ");
             string model = Console.ReadLine();
             Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
             Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
-            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter price per hour: ");
+            double priceHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter price per day: ");
+            double priceDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
-            Console.WriteLine("Enter price per hour: ");
-            double priceHour = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter price per day: ");
-            double priceDay = double.Parse(Console.ReadLine());
+            RentalService rentalService = new RentalService(priceHour, priceDay);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice: ");
+            Console.WriteLine(carRental.Invoice);
 
         }
     }
